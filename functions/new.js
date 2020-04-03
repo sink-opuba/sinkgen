d1b770f74bd625b43b8b864a87244b9011b2c470
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2020-04-03 11:52:41 +0100

    add new.js severless function

--- functions/new.js
+++ functions/new.js
@@ -9,3 +9,28 @@ const q = faunadb.query;
 const client = new faunadb.Client({
   secret: process.env.FAUNADB
 });
+
+module.exports.hander = async event => {
+  const data = querystring.parse(event.body);
+  const uniquePath = shortid.generate();
+  data.path = uniquePath;
+
+  try {
+    const queryResponse = await client.query(
+      q.Create(q.Collection("page"), {
+        data: data
+      })
+    );
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(queryResponse)
+    };
+    return response;
+  } catch (error) {
+    const errorResponse = {
+      statusCode: 400,
+      body: JSON.stringify(error)
+    };
+    return errorResponse;
+  }
+};
